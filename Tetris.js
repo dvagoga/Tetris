65e722c302a04d64b04fdd5d34a4079f2659ef3d
dvagoga <dvagoga@gmail.com>
2014-01-07 23:38:30 +0400

    add moving of point

--- Tetris.js
+++ Tetris.js
@@ -1,5 +1,59 @@
+"use strict";
+// classes
+function onePiece(x){
+   var ox = x;
+   this.getX = function(){
+      return ox;
+   };
+   this.goLeft = function(){
+      if (ox > 0){
+	     ox--;
+	  };
+   };
+   this.goRight = function(){
+      if (ox < (tetField.width*tetField.height)-1){
+         ox++;
+      };
+   };
+   this.goUp = function(){
+      if (ox > (tetField.height-1)){
+         ox = ox - tetField.height;
+      };      
+   };
+   this.goDown = function(){
+      if (ox < tetField.width*(tetField.height-1)){
+         ox = ox + tetField.height;
+      };      
+   };
+};
+// user interface
+function fUp(){
+   document.getElementById(point.getX()).className = 'kubNul';
+   point.goUp();
+   var newPoint = document.getElementById(point.getX());
+   newPoint.className = 'kube';
+};
+function fDown(){
+   document.getElementById(point.getX()).className = 'kubNul';
+   point.goDown();
+   var newPoint = document.getElementById(point.getX());
+   newPoint.className = 'kube';
+};
+function fLeft(){
+   document.getElementById(point.getX()).className = 'kubNul';
+   point.goLeft();
+   var newPoint = document.getElementById(point.getX());
+   newPoint.className = 'kube';
+};
+function fRight(){
+   document.getElementById(point.getX()).className = 'kubNul';
+   point.goRight();
+   var newPoint = document.getElementById(point.getX());
+   newPoint.className = 'kube';
+};
+// initialization graphics
 function initGameField(width, height){
-   var pole = document.getElementById('gameField');         
+   var pole = document.getElementById('gameField');
    var gField = document.createElement('div');
    pole.appendChild(gField);
    for (var i=0; i<width; i++){
@@ -13,6 +67,10 @@ function initGameField(width, height){
       wall.className = 'wall';
       gField.appendChild(wall);
    };
+   document.getElementById('bUp').onclick = fUp;
+   document.getElementById('bDown').onclick = fDown;
+   document.getElementById('bLeft').onclick = fLeft;
+   document.getElementById('bRight').onclick = fRight;
 };
 
 var tetField = {
@@ -22,4 +80,12 @@ var tetField = {
 
 initGameField(tetField.width, tetField.height);
 
-document.getElementById(tetField.width*tetField.height-10).className = 'kube';
+var point = new onePiece(tetField.height-12);
+document.getElementById(point.getX()).className = 'kube';
+
+var timeCnt = new Date;
+var win = false;
+
+while(!win){
+   win = true;
+};
